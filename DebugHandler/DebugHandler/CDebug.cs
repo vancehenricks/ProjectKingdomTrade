ee24a43319cac947770a2ee85421c5e1282a9978
Vance Henricks Patual <vpatual@gmail.com>
2021-04-26 17:32:30 +0800

    Added copyright

--- DebugHandler/DebugHandler/CDebug.cs
+++ DebugHandler/DebugHandler/CDebug.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/* Copyright 2021 by Vance Henricks Patual - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Vance Henricks Patual <vpatual@gmail.com>, April 2021
+ */
+
+using System;
 using UnityEngine;
 
 namespace DebugHandler
