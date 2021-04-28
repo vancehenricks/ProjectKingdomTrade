f610fa850f85cd4da03be985863bac8414e6ca5f
Vance Henricks Patual <vpatual@gmail.com>
2021-04-28 17:38:43 +0800

    get-revision will now execute during building

--- Assets/Editor/VersionHandler.cs
+++ Assets/Editor/VersionHandler.cs
@@ -7,14 +7,19 @@ using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
-class VersionHandler : IPostprocessBuildWithReport
+class VersionHandler : IPreprocessBuildWithReport
 {
     public int callbackOrder { get { return 0; } }
 
-    public void OnPostprocessBuild(BuildReport report)
+    public void OnPreprocessBuild(BuildReport report)
     {
-        string revision = File.ReadAllText("revision");
+        string path = Application.dataPath.Remove(Application.dataPath.IndexOf("Assets"),@"Assets".Length);
 
+        CDebug.Log(this, "get-revision.bat path=" +  path, LogType.Warning);
+
+        Process.Start("cmd.exe", "/C " + path +  @"\get-revision.bat");
+
+        string revision = File.ReadAllText("revision");
         string date = DateTime.UtcNow.ToString("ddMMyyyy");
         string version = date + "." + revision;
 
